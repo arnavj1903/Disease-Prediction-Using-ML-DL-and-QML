6f35fe312cc95ca2e7e7390711dfce931b2c7279
Aniketh <anikethbhargav05@gmail.com>
2025-04-16 14:12:52 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -321,6 +321,6 @@ def predict(disease):
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run(host="0.0.0.0", port=5000, debug=True)
     except Exception as e:
         logger.error(f"Application failed to start: {str(e)}")
